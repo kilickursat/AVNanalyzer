b5b975d8fe8b9b5a4c80ebba4332eea1de25aa5d
Kursat Kilic <kilic_kursat@hotmail.com>
2024-10-14 16:07:40 +0200

    Update avn.py

--- avn.py
+++ avn.py
@@ -110,7 +110,7 @@ def calculate_derived_features(df, working_pressure_col, revolution_col, n1, tor
             df['Average Speed (mm/min)'] = average_speed
             
             if revolution_col is not None:
-                df['Penetration Rate [mm/rev]'] = df.apply(lambda row: calculate_penetration_rate(row, revolution_col), axis=1)
+                df['Penetration Rate [mm/rev]'] = df.apply(lambda row: calculate_penetration_rate(row), axis=1)
         
         return df
         
@@ -807,21 +807,6 @@ def calculate_advance_rate_and_stats(df, distance_column, time_column):
         return None, 0
 
 
-def calculate_penetration_rate(row, revolution_col, advance_rate_col):
-    try:
-        speed = row[advance_rate_col]
-        revolution = row[revolution_col]
-
-        if pd.isna(speed) or pd.isna(revolution):
-            return np.nan
-        elif revolution == 0:
-            return np.inf if speed != 0 else 0
-        else:
-            return round(speed / revolution, 4)
-    except Exception as e:
-        st.error(f"Error calculating penetration rate: {str(e)}")
-        return np.nan
-
 # Main function
 def main():
     try:
@@ -874,7 +859,7 @@ def main():
                     
                     if 'Average Speed (mm/min)' in df.columns:
                         df['Penetration Rate [mm/rev]'] = df.apply(
-                            lambda row: calculate_penetration_rate(row, revolution_col), axis=1
+                            lambda row: calculate_penetration_rate(row), axis=1
                         )
 
                 df_viz = rename_columns(df.copy(), working_pressure_col, revolution_col, selected_distance, advance_rate_col)
